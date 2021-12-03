a9c474041f9f02289181bcaf1750d93bc32fd90c
Jack Gashler <jgashler44@gmail.com>
2021-12-03 00:04:00 -0700

    simplify functions and tighten up memory, what you need is what you get

--- BYUMech_Color_Sorter/BYUMech_Color_Sorter.ino
+++ BYUMech_Color_Sorter/BYUMech_Color_Sorter.ino
@@ -2,40 +2,12 @@
 #include <Servo.h>
 #include "Adafruit_TCS34725.h"
 
-Servo mainServo, RServo, GServo, BServo, YServo;
+Servo mainServo, RServo, GServo, BServo;
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_600MS, TCS34725_GAIN_1X /*1X, 4X, 16X, 60X*/);
 
-#define PRINT_SERIAL
+//#define PRINT_SERIAL
 
 enum Color {RED, GREEN, BLUE, YELLOW};
-
-#define RUR_BOUND 1
-#define RLR_BOUND 1
-#define RUG_BOUND 1
-#define RLG_BOUND 1
-#define RUB_BOUND 1
-#define RLB_BOUND 1
-
-#define GUR_BOUND 1
-#define GLR_BOUND 1
-#define GUG_BOUND 1
-#define GLG_BOUND 1
-#define GUB_BOUND 1
-#define GLB_BOUND 1
-
-#define BUR_BOUND 1
-#define BLR_BOUND 1
-#define BUG_BOUND 1
-#define BLG_BOUND 1
-#define BUB_BOUND 1
-#define BLB_BOUND 1
-
-#define YUR_BOUND 1
-#define YLR_BOUND 1
-#define YUG_BOUND 1
-#define YLG_BOUND 1
-#define YUB_BOUND 1
-#define YLB_BOUND 1
  
 void setup(void) {
   Serial.begin(9600); 
@@ -62,9 +34,6 @@ void setup(void) {
   
   BServo.attach(11);
   BServo.write(0);
-  
-//  YServo.attach(5);
-//  YServo.write(0);
 //#endif
 
 }
@@ -75,26 +44,7 @@ void loop(void) {
   //PING dispenser
   delay(5000);
   tcs.getRawData(&r, &g, &b, &c);
-  Color color = colorMath(r,g,b,tcs,0);
-  
-  //when a color is sensed, dropBall(color)
-//  if (color == RED) {
-//    dropBall(RServo);
-//    goto PING;
-//  }
-//  else if (color == GREEN) {
-//    dropBall(GServo);
-//    goto PING;
-//  }
-//  else if (color == BLUE) {
-//    dropBall(BServo);
-//    goto PING;
-//  }
-//  else if (color == YELLOW) {
-//    dropBall(YServo);
-//    goto PING;
-//  }
-
+  Color color = colorMath(r,g,b);
   dropBall(color);
 }
 
@@ -110,45 +60,21 @@ void dropBall(Color color) {
   if (color == RED) RServo.write(90);
   else if (color == GREEN) GServo.write(0);
   else if (color == BLUE) BServo.write(0);
-
-  delay(50);
-  mainServo.write(0);
+  mainServo.write(0);  
 }
 
-Color colorMath(uint16_t red, uint16_t green, uint16_t blue, Adafruit_TCS34725 tcs, int call) {
-  ++call;
-  if (call > 5) {
-    Serial.println("ERROR in colorMath");
-    return RED;
-  }
+Color colorMath(uint16_t red, uint16_t green, uint16_t blue) {
   Color color;
   double tot = sqrt((double)(red*red + green*green + blue*blue));
   double r = red/tot;
   double g = green/tot;
   double b = blue/tot;
 
-//  //check to see if the current rbg value falls within the defined bounds of a color
-//  if (r > RLR_BOUND && r < RUR_BOUND && g > RLG_BOUND && g < RUG_BOUND && b > RLB_BOUND && b < RUB_BOUND) color = RED;
-//  else if (r > GLR_BOUND && r < GUR_BOUND && g > GLG_BOUND && g < GUG_BOUND && b > GLB_BOUND && b < GUB_BOUND) color = GREEN;
-//  else if (r > BLR_BOUND && r < BUR_BOUND && g > BLG_BOUND && g < BUG_BOUND && b > BLB_BOUND && b < BUB_BOUND) color = BLUE;
-//  else if (r > YLR_BOUND && r < YUR_BOUND && g > YLG_BOUND && g < YUG_BOUND && b > YLB_BOUND && b < YUB_BOUND) color = YELLOW;
-  
-//  //if it did not scan within a bound, recursive call and retry
-//  else {
-//    uint16_t nr, ng, nb, nc;
-//    tcs.getRawData(&nr, &ng, &nb, &nc);
-//    color = colorMath(nr, ng, nb, tcs, call);
-//  }
-
-if (b > r && b > g) color = BLUE;
-else if (r / b > 2 && g / b > 2) color = YELLOW;
-else if (r > b && r > g) color = RED;
-else if (g > r && g > b) color = GREEN;
-  else {
-    uint16_t nr, ng, nb, nc;
-    tcs.getRawData(&nr, &ng, &nb, &nc);
-    color = colorMath(nr, ng, nb, tcs, call);
-  }
+  if (b > r && b > g) color = BLUE;
+  else if (r / b > 2 && g / b > 2) color = YELLOW;
+  else if (r > b && r > g) color = RED;
+  else if (g > r && g > b) color = GREEN;
+  else color = RED;
 
 #ifdef PRINT_SERIAL
   //print data to serial
